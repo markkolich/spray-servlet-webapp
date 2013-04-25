fcaa41e3516d3519d4cb81edfe71392e431072a3
Mark Kolich <social@kolich.com>
2013-04-25 09:15:24 -0700

    moving stuff around

--- src/main/scala/com/kolich/spray/service/WebAppService.scala
+++ src/main/scala/com/kolich/spray/service/WebAppService.scala
@@ -4,6 +4,8 @@ import scala.concurrent.duration._
 
 import com.weiglewilczek.slf4s.{Logging, Logger}
 
+import com.kolich.spray.templating._
+
 import akka.actor._
 import spray.util._
 import spray.http._
@@ -11,13 +13,21 @@ import spray.routing._
 import MediaTypes._
 import HttpMethods._
 
-class WebAppService extends Actor with HttpServiceActor with Logging {
+class WebAppService extends Actor with HttpServiceActor with ScalateSupport with Logging {
   
   @Override
   def receive = runRoute {
     path("") {
       get {
-        complete("Hola!")
+        render("templates/index.ssp")
+      }
+    } ~
+    path ("login") {
+      get {
+        render("templates/login.ssp")
+      } ~
+      post {
+        complete("Meh")
       }
     }
   }
---
+++ src/main/scala/com/kolich/spray/templating/ScalateSupport.scala
@@ -0,0 +1,27 @@
+package com.kolich.spray.templating
+
+import org.fusesource.scalate.{Binding, TemplateEngine}
+import spray.http._
+import spray.routing._
+import MediaTypes._
+
+// Totally borrowed from:
+// https://github.com/spray/spray/blob/release-1.0.x/spray-routing/src/main/scala/spray/routing/directives/ScalateSupport.scala
+// I was expecting this to be part of Spray 1.1-M7, but I couldn't find it there.
+trait ScalateSupport {
+  
+  import Directives._
+  
+  private val templateEngine = new TemplateEngine
+    
+  def render(uri: String, attributes: Map[String,Any] = Map.empty,
+             extraBindings: Traversable[Binding] = Nil,
+             mediaType: MediaType = `text/html`): Route = {
+    respondWithMediaType(mediaType) {
+      complete {
+        templateEngine.layout(uri, attributes, extraBindings)
+      }
+    }
+  }
+
+}
