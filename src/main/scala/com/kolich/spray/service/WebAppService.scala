eb3cf611389cdf3001f6e9b7c311e24e9e2ff5ae
Mark Kolich <social@kolich.com>
2013-05-13 21:58:47 -0700

    comments gooooooood

--- src/main/scala/com/kolich/spray/service/WebAppService.scala
+++ src/main/scala/com/kolich/spray/service/WebAppService.scala
@@ -1,3 +1,29 @@
+/**
+ * Copyright (c) 2013 Mark S. Kolich
+ * http://mark.koli.ch
+ *
+ * Permission is hereby granted, free of charge, to any person
+ * obtaining a copy of this software and associated documentation
+ * files (the "Software"), to deal in the Software without
+ * restriction, including without limitation the rights to use,
+ * copy, modify, merge, publish, distribute, sublicense, and/or sell
+ * copies of the Software, and to permit persons to whom the
+ * Software is furnished to do so, subject to the following
+ * conditions:
+ *
+ * The above copyright notice and this permission notice shall be
+ * included in all copies or substantial portions of the Software.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+ * FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ */
+
 package com.kolich.spray.service
 
 import scala.concurrent.duration._
@@ -68,8 +94,7 @@ class WebAppService extends Actor with HttpServiceActor with Secure with Scalate
         	  }
         	  session match {
         	    case Some(session) => {
-        	    	logger.info("Created session (login), session ID: " +
-        	    	    session.id)
+        	    	logger.info("Created session (login), session ID: " + session)
         	    	sessionCache.setSession(session.id, session)
         	    	respondWithHeader(getSessionCookieHeader(content = session.id)) {
         	    		_.complete(getRedirectToRoute("/home"))
@@ -101,7 +126,7 @@ class WebAppService extends Actor with HttpServiceActor with Secure with Scalate
                 sessionCache.removeSession(sessionId) match {
                   case Some(session:SessionData) => {
                 	  logger.info("Removed session (logout) from " +
-                	      "session cache: " + session.id)
+                	      "session cache: " + session)
                   }
                   case _ => {
                 	  // Ignore, silently.
