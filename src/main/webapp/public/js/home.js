27280fe9393cd6b2a67e900bf059d0ea2992ce2d
Mark Kolich <social@kolich.com>
2013-05-22 16:47:57 -0700

    removing ExtJS login page, cleaned up application home, provided further descriptions on what's coming from where

--- src/main/webapp/public/js/home.js
+++ src/main/webapp/public/js/home.js
@@ -1,4 +1,54 @@
+Ext.define('SimpsonStore', {
+	extend : 'Ext.data.Store',
+	fields : [ 'name', 'email', 'phone' ],
+	data : {
+		'items' : [ {
+			'name' : 'Lisa',
+			'email' : 'lisa@simpsons.com',
+			'phone' : '555-111-1224'
+		}, {
+			'name' : 'Bart',
+			'email' : 'bart@simpsons.com',
+			'phone' : '555-222-1234'
+		}, {
+			'name' : 'Homer',
+			'email' : 'home@simpsons.com',
+			'phone' : '555-222-1244'
+		}, {
+			'name' : 'Marge',
+			'email' : 'marge@simpsons.com',
+			'phone' : '555-222-1254'
+		} ]
+	},
+	proxy : {
+		type : 'memory',
+		reader : {
+			type : 'json',
+			root : 'items'
+		}
+	}
+});
 
-Ext.onDocumentReady(function() {
-	// todo ...
+Ext.define('SimpsonGrid', {
+	extend : 'Ext.grid.Panel',
+	xtype : 'demo-grid',
+	store : Ext.create('SimpsonStore'),
+	columns : [ {
+		text : 'Name',
+		dataIndex : 'name'
+	}, {
+		text : 'Email',
+		dataIndex : 'email',
+		flex : 1
+	}, {
+		text : 'Phone',
+		dataIndex : 'phone'
+	} ],
+	height : 200,
+	width : 500,
+	renderTo : 'gridpanel-demo'
+});
+
+Ext.onReady(function() {
+	Ext.create('SimpsonGrid');
 });
